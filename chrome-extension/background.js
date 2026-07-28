b6454e087227fb34dde3b25580a567de574eb4eb
Sachin Thakur <officialsachinthakur1@gmail.com>
2026-07-28 15:39:09 +0530

    Remove Chrome Extension directory as requested by user

--- chrome-extension/background.js
+++
@@ -1,4 +0,0 @@
-// Background service worker for GetSetMart Seller Assistant
-chrome.runtime.onInstalled.addListener(() => {
-  console.log("GetSetMart E-Commerce Seller Assistant Chrome Extension installed successfully!");
-});
